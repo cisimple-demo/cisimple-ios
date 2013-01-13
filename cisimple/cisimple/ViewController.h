b127343bc26b5994a7aa10a6e5049adcd65b1a4b
Kevin Rohling <kevin@kevinrohling.com>
2013-01-13 15:23:59 -0800

    intentionally breaking the build

--- cisimple/cisimple/ViewController.h
+++ cisimple/cisimple/ViewController.h
@@ -5,7 +5,7 @@
 //  Created by Kevin Rohling on 6/17/12.
 //  Copyright (c) 2012 card.io. All rights reserved.
 //  
-
+Intentionally breaking the build
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
