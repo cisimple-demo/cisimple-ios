4418bd71d5619da0d7b05e71264b2e724b0cbd24
Kevin Rohling <kevin@kevinrohling.com>
2013-01-13 15:02:11 -0800

    This is a test commit

--- automation.js
+++ automation.js
@@ -2,3 +2,5 @@ var target = UIATarget.localTarget();
 var app = target.frontMostApp();
 var window = app.mainWindow();
 window.buttons()["Tap Me"].tap();
+
+//Added comment
--- cisimple/cisimple/ViewController.m
+++ cisimple/cisimple/ViewController.m
@@ -5,6 +5,8 @@
 //  Created by Kevin Rohling on 6/17/12.
 //  Copyright (c) 2012 card.io. All rights reserved.
 //
+//
+//
 
 #import "ViewController.h"
 #import <Crashlytics/Crashlytics.h>
